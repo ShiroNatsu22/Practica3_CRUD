6d2e46d2dac797c50534d9f5b20ba0668a0ee9d6
ShiroNatsu22 <aushojgs@gmail.com>
2017-01-25 13:28:03 +0100

    UserDAO i RolDAO començats

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -2,10 +2,16 @@ package dao;
 
 import pojo.User;
 
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by jgarcias on 24/01/17.
  */
 public interface UserDAO {
-    User findUser(String name);
-    User findUser(String name, boolean fillRoll);
+    User findUser(String userName);
+    List<User> findUser(String name, boolean fillRoll);
+    User deleteUser(String name);
+    void insertUser(String name,String password,String[] roles);
+    List<User> getUsers() ;
 }
